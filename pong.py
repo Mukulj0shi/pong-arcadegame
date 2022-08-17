f7db8f26504525e485eddb34a4c821af73cdf532
Mukulj0shi <77253989+Mukulj0shi@users.noreply.github.com>
2022-08-17 20:31:52 +0530

    make the ball bounce, self made method

--- pong.py
+++ pong.py
@@ -4,27 +4,22 @@ from turtle import Turtle
 class ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("square")
-        self.shapesize(0.5, 0.5)
+        self.shape("circle")
+        self.circle(20)
         self.color("blue")
         self.penup()
 
 
+
+
     def move_ball(self):
-        if self.heading() == 0 and self.xcor() < 380:
-            self.forward(10)
-            print(self.position())
-        elif self.xcor() == 380:
-            self.setheading(180)
-            self.forward(1)
-            print(self.heading())
-        elif self.heading() == 180.0 and self.xcor() >= -380:
-            self.forward(10)
-            print(self.position())
-        elif self.xcor() == -381:
-            self.setheading(0)
-            self.forward(1)
-            print(self.heading())
+        #self.left(45)
+        self.goto(362, 260)  # Y is a variable
+        #self.setheading(180)
+        self.goto(0, 0)
+        self.goto(-365, 260)
+
+
 
 
 
